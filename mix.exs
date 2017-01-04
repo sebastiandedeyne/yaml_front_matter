5ab778717ecff48b392967196b2d8f686f335cc3
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2017-01-04 08:42:04 +0100

    Lowered elixir req

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule YamlFrontMatter.Mixfile do
   def project do
     [app: :yaml_front_matter,
      version: "0.1.0",
-     elixir: "~> 1.3",
+     elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
