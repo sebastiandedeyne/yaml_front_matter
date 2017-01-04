a41539b797325a6c6c90c19b5d9b8a33d1e3e647
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2017-01-04 08:47:05 +0100

    Mix

--- mix.exs
+++ mix.exs
@@ -7,7 +7,14 @@ defmodule YamlFrontMatter.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     description: "A YAML front matter parser for Elixir.",
+     package: [
+       name: :yaml_front_matter,
+       files: ["lib", "priv", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],
+       maintainers: ["Sebastian De Deyne"],
+       licenses: ["MIT"],
+       links: %{"GitHub" => "https://github.com/sebastiandedeyne/yaml_front_matter"}],
+     deps: [{:yaml_elixir, "~> 1.3"}]]
   end
 
   # Configuration for the OTP application
@@ -15,18 +22,5 @@ defmodule YamlFrontMatter.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     [applications: [:yaml_elixir]]
-  end
-
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
-  defp deps do
-    [{:yaml_elixir, "~> 1.3"}]
   end
 end
