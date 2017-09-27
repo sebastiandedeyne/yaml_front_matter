9ac7465bcac1e4db425e0184f265daa0c0abb338
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2017-09-27 13:31:43 +0200

    Bump version req

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule YamlFrontMatter.Mixfile do
   def project do
     [app: :yaml_front_matter,
      version: @version,
-     elixir: "~> 1.3",
+     elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      description: "A YAML front matter parser for Elixir.",
